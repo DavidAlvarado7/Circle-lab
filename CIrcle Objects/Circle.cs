b7d66c6d76da9e69ca857abb4c63880f810c4090
DavidAlvarado7 <144187066+DavidAlvarado7@users.noreply.github.com>
2023-11-06 18:07:37 -0500

    Add files via upload

---
+++ CIrcle Objects/Circle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace CIrcle_Objects
+{
+	public class Circle
+	{
+		public Circle(double radius)
+		{
+			Grow(_radius);
+
+			_radius = radius;
+			
+		}
+
+		
+		private double _radius { get; set; }
+
+
+
+		public double GetDiameterOfCircle()
+		{
+			double sum = 2 * _radius;
+			return sum;
+		}
+
+		public double GetAreaOfCircle()
+		{
+			double sum = _radius +_radius * 3.14;
+				return sum;
+
+		}
+
+		public double CalculateCircumference()
+		{
+			double radius = 2 * 3.14 * 2; 
+			return radius;
+		}
+
+		public void Grow(double radius)
+
+		{
+			radius = 2 * _radius;
+
+			
+		}
+
+		
+
+
+	} 
+}
+
---
+++ CIrcle Objects/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using CIrcle_Objects;
+
+Console.WriteLine("Hello, please enter a number radius.");
+while (true)
+{
+
+
+
+    string userCircle = Console.ReadLine();
+    if (double.TryParse(userCircle, out double number))
+    {
+        Circle circle = new Circle(number);
+        Console.WriteLine(circle.GetDiameterOfCircle());
+        Console.WriteLine(circle.GetAreaOfCircle());
+        Console.WriteLine(circle.CalculateCircumference());
+
+        Console.WriteLine("should the circle grow?");
+        string answer = Console.ReadLine();
+        while (true)
+            if (answer == "Y")
+            {
+                Console.WriteLine("The circle is growing bruh");
+                Console.WriteLine(circle.Grow);
+                break;
+
+            }
+
+
+
+
+    }
+    else
+    {
+        Console.WriteLine("Please, enter a valid number radius.");
+    }
+
+}
+
